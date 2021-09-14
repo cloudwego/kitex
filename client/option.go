5c3eeed3e9a5791e466962735846ecaf2f268d0f
lushaojie <lushaojie@bytedance.com>
2021-09-14 17:02:12 +0800

    fix(client): add RPCTimeout middleware only required

--- client/option.go
+++ client/option.go
@@ -239,6 +239,7 @@ func WithRPCTimeout(d time.Duration) Option {
 
 		rpcinfo.AsMutableRPCConfig(o.Configs).SetRPCTimeout(d)
 		o.Locks.Bits |= rpcinfo.BitRPCTimeout
+		o.CheckRPCTimeout = true
 	}}
 }
 
@@ -261,6 +262,7 @@ func WithTimeoutProvider(p rpcinfo.TimeoutProvider) Option {
 	return Option{F: func(o *client.Options, di *utils.Slice) {
 		di.Push(fmt.Sprintf("WithTimeoutProvider(%T(%+v))", p, p))
 		o.Timeouts = p
+		o.CheckRPCTimeout = true
 	}}
 }
 
