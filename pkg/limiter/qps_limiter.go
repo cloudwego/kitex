caeef42ee03cb214cab8810b1078642f629eb54a
YangruiEmma <yangr.emma@gmail.com>
2021-09-23 14:31:20 +0800

    fix: goroutine leak when update interval of qpsLimiter (#140)

--- pkg/limiter/qps_limiter.go
+++ pkg/limiter/qps_limiter.go
@@ -25,11 +25,12 @@ var fixedWindowTime = time.Second
 
 // qpsLimiter implements the RateLimiter interface.
 type qpsLimiter struct {
-	limit    int32
-	tokens   int32
-	interval time.Duration
-	once     int32
-	ticker   *time.Ticker
+	limit      int32
+	tokens     int32
+	interval   time.Duration
+	once       int32
+	ticker     *time.Ticker
+	tickerDone chan bool
 }
 
 // NewQPSLimiter creates qpsLimiter.
@@ -39,9 +40,8 @@ func NewQPSLimiter(interval time.Duration, limit int) RateLimiter {
 		tokens:   int32(limit),
 		interval: interval,
 		once:     calcOnce(interval, limit),
-		ticker:   time.NewTicker(interval),
 	}
-	go l.startTicker()
+	go l.startTicker(interval)
 	return l
 }
 
@@ -62,8 +62,7 @@ func (l *qpsLimiter) UpdateQPSLimit(interval time.Duration, limit int) {
 	if interval != l.interval {
 		l.interval = interval
 		l.stopTicker()
-		l.ticker = time.NewTicker(interval)
-		go l.startTicker()
+		go l.startTicker(interval)
 	}
 }
 
@@ -83,15 +82,31 @@ func (l *qpsLimiter) Status() (max, cur int, interval time.Duration) {
 	return
 }
 
-func (l *qpsLimiter) startTicker() {
-	ch := l.ticker.C
-	for range ch {
-		l.updateToken()
+func (l *qpsLimiter) startTicker(interval time.Duration) {
+	l.ticker = time.NewTicker(interval)
+	defer l.ticker.Stop()
+	l.tickerDone = make(chan bool, 1)
+	tc := l.ticker.C
+	td := l.tickerDone
+	// ticker and tickerDone can be reset, cannot use l.ticker or l.tickerDone directly
+	for {
+		select {
+		case <-tc:
+			l.updateToken()
+		case <-td:
+			return
+		}
 	}
 }
 
 func (l *qpsLimiter) stopTicker() {
-	l.ticker.Stop()
+	if l.tickerDone == nil {
+		return
+	}
+	select {
+	case l.tickerDone <- true:
+	default:
+	}
 }
 
 // Some deviation is allowed here to gain better performance.
