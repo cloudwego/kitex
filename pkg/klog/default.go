db161b1fe93fafc7c7e235d05a9c9b8f931f1a98
lushaojie <lushaojie@bytedance.com>
2021-12-02 15:19:03 +0800

    refactor(klog): wraps default logger's method instead of using function variables

--- pkg/klog/default.go
+++ pkg/klog/default.go
@@ -52,32 +52,110 @@ func SetLogger(v FullLogger) {
 	defaultLogger = v
 }
 
-// Global functions.
-// Use function variables rather than function wrappers to prevent log
-// depth from changing.
-var (
-	Trace      = defaultLogger.Trace
-	Debug      = defaultLogger.Debug
-	Info       = defaultLogger.Info
-	Notice     = defaultLogger.Notice
-	Warn       = defaultLogger.Warn
-	Error      = defaultLogger.Error
-	Fatal      = defaultLogger.Fatal
-	Tracef     = defaultLogger.Tracef
-	Debugf     = defaultLogger.Debugf
-	Infof      = defaultLogger.Infof
-	Noticef    = defaultLogger.Noticef
-	Warnf      = defaultLogger.Warnf
-	Errorf     = defaultLogger.Errorf
-	Fatalf     = defaultLogger.Fatalf
-	CtxTracef  = defaultLogger.CtxTracef
-	CtxDebugf  = defaultLogger.CtxDebugf
-	CtxInfof   = defaultLogger.CtxInfof
-	CtxNoticef = defaultLogger.CtxNoticef
-	CtxWarnf   = defaultLogger.CtxWarnf
-	CtxErrorf  = defaultLogger.CtxErrorf
-	CtxFatalf  = defaultLogger.CtxFatalf
-)
+// Fatal calls the default logger's Fatal method and then os.Exit(1).
+func Fatal(v ...interface{}) {
+	defaultLogger.Fatal(v...)
+}
+
+// Error calls the default logger's Error method.
+func Error(v ...interface{}) {
+	defaultLogger.Error(v...)
+}
+
+// Warn calls the default logger's Warn method.
+func Warn(v ...interface{}) {
+	defaultLogger.Warn(v...)
+}
+
+// Notice calls the default logger's Notice method.
+func Notice(v ...interface{}) {
+	defaultLogger.Notice(v...)
+}
+
+// Info calls the default logger's Info method.
+func Info(v ...interface{}) {
+	defaultLogger.Info(v...)
+}
+
+// Debug calls the default logger's Debug method.
+func Debug(v ...interface{}) {
+	defaultLogger.Debug(v...)
+}
+
+// Trace calls the default logger's Trace method.
+func Trace(v ...interface{}) {
+	defaultLogger.Trace(v...)
+}
+
+// Fatalf calls the default logger's Fatalf method and then os.Exit(1).
+func Fatalf(format string, v ...interface{}) {
+	defaultLogger.Fatalf(format, v...)
+}
+
+// Errorf calls the default logger's Errorf method.
+func Errorf(format string, v ...interface{}) {
+	defaultLogger.Errorf(format, v...)
+}
+
+// Warnf calls the default logger's Warnf method.
+func Warnf(format string, v ...interface{}) {
+	defaultLogger.Warnf(format, v...)
+}
+
+// Noticef calls the default logger's Noticef method.
+func Noticef(format string, v ...interface{}) {
+	defaultLogger.Noticef(format, v...)
+}
+
+// Infof calls the default logger's Infof method.
+func Infof(format string, v ...interface{}) {
+	defaultLogger.Infof(format, v...)
+}
+
+// Debugf calls the default logger's Debugf method.
+func Debugf(format string, v ...interface{}) {
+	defaultLogger.Debugf(format, v...)
+}
+
+// Tracef calls the default logger's Tracef method.
+func Tracef(format string, v ...interface{}) {
+	defaultLogger.Tracef(format, v...)
+}
+
+// CtxFatalf calls the default logger's CtxFatalf method and then os.Exit(1).
+func CtxFatalf(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxFatalf(ctx, format, v...)
+}
+
+// CtxErrorf calls the default logger's CtxErrorf method.
+func CtxErrorf(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxErrorf(ctx, format, v...)
+}
+
+// CtxWarnf calls the default logger's CtxWarnf method.
+func CtxWarnf(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxWarnf(ctx, format, v...)
+}
+
+// CtxNoticef calls the default logger's CtxNoticef method.
+func CtxNoticef(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxNoticef(ctx, format, v...)
+}
+
+// CtxInfof calls the default logger's CtxInfof method.
+func CtxInfof(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxInfof(ctx, format, v...)
+}
+
+// CtxDebugf calls the default logger's CtxDebugf method.
+func CtxDebugf(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxDebugf(ctx, format, v...)
+}
+
+// CtxTracef calls the default logger's CtxTracef method.
+func CtxTracef(ctx context.Context, format string, v ...interface{}) {
+	defaultLogger.CtxTracef(ctx, format, v...)
+}
 
 type localLogger struct {
 	logger *log.Logger
