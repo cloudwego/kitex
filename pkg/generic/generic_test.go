cfbc1e147a857d7688c99e4b10065a7f2dbeab3c
scotty <scotty_lzw@163.com>
2022-04-08 00:42:15 +0800

    feat: generic test

---
+++ pkg/generic/generic_test.go
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2021 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package generic
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"github.com/cloudwego/kitex/internal/test"
+	"github.com/cloudwego/kitex/pkg/serviceinfo"
+)
+
+func TestBinaryThriftGeneric(t *testing.T) {
+	g := BinaryThriftGeneric()
+	defer g.Close()
+
+	test.Assert(t, g.Framed() == true)
+
+	test.Assert(t, g.PayloadCodecType() == serviceinfo.Thrift)
+
+	method, err := g.GetMethod(nil, "Test")
+	test.Assert(t, err == nil, err)
+	test.Assert(t, method != nil)
+
+	t.Log(g.PayloadCodec().Name())
+}
+
+func TestMapThriftGeneric(t *testing.T) {
+	p, err := NewThriftFileProvider("./map_test/idl/mock.thrift")
+	test.Assert(t, err == nil, err)
+
+	// new
+	g, err := MapThriftGeneric(p)
+	test.Assert(t, err == nil, err)
+	defer g.Close()
+
+	test.Assert(t, g.Framed() == false)
+
+	test.Assert(t, g.PayloadCodecType() == serviceinfo.Thrift)
+
+	method, err := g.GetMethod(nil, "Test")
+	test.Assert(t, err == nil, err)
+	test.Assert(t, method != nil)
+
+	t.Log(g.PayloadCodec().Name())
+}
+
+func TestHTTPThriftGeneric(t *testing.T) {
+	p, err := NewThriftFileProvider("./http_test/idl/binary_echo.thrift")
+	test.Assert(t, err == nil, err)
+
+	// new
+	g, err := HTTPThriftGeneric(p)
+	test.Assert(t, err == nil, err)
+	defer g.Close()
+
+	err = SetBinaryWithBase64(g, true)
+	test.Assert(t, err == nil, err)
+
+	test.Assert(t, g.Framed() == false)
+
+	test.Assert(t, g.PayloadCodecType() == serviceinfo.Thrift)
+
+	url := "https://example.com/BinaryEcho"
+	body := map[string]interface{}{
+		"msg": "Test",
+	}
+	data, err := json.Marshal(body)
+	test.Assert(t, err == nil)
+	// NewRequest
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(data))
+	test.Assert(t, err == nil)
+	customReq, err := FromHTTPRequest(req)
+	test.Assert(t, err == nil, err)
+
+	method, err := g.GetMethod(customReq, "Test")
+	test.Assert(t, err == nil, err)
+	test.Assert(t, method != nil)
+
+	t.Log(g.PayloadCodec().Name())
+}
+
+func TestJSONThriftGeneric(t *testing.T) {
+	p, err := NewThriftFileProvider("./json_test/idl/mock.thrift")
+	test.Assert(t, err == nil, err)
+
+	g, err := JSONThriftGeneric(p)
+	test.Assert(t, err == nil, err)
+	defer g.Close()
+
+	test.Assert(t, g.Framed() == false)
+
+	test.Assert(t, g.PayloadCodecType() == serviceinfo.Thrift)
+
+	method, err := g.GetMethod(nil, "Test")
+	test.Assert(t, err == nil, err)
+	test.Assert(t, method != nil)
+
+	t.Log(g.PayloadCodec().Name())
+}
