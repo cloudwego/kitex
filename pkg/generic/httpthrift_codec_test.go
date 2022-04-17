832e6c8173d8da2743ad28e8c448986614612c34
scotty <scotty_lzw@163.com>
2022-04-18 00:55:11 +0800

    feat: http thrift codec test

--- pkg/generic/httpthrift_codec_test.go
+++ pkg/generic/httpthrift_codec_test.go
@@ -18,11 +18,17 @@ package generic
 
 import (
 	"bytes"
+	"context"
 	stdjson "encoding/json"
 	"net/http"
 	"testing"
 
+	"github.com/cloudwego/kitex/internal/mocks"
 	"github.com/cloudwego/kitex/internal/test"
+	"github.com/cloudwego/kitex/pkg/generic/descriptor"
+	"github.com/cloudwego/kitex/pkg/remote"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/transport"
 )
 
 func TestFromHTTPRequest(t *testing.T) {
@@ -35,4 +41,65 @@ func TestFromHTTPRequest(t *testing.T) {
 		"a": stdjson.Number("1111111111111"),
 		"b": "hello",
 	})
+}
+
+func TestHttpThriftCodec(t *testing.T) {
+	p, err := NewThriftFileProvider("./http_test/idl/binary_echo.thrift")
+	test.Assert(t, err == nil)
+	htc, err := newHTTPThriftCodec(p, thriftCodec)
+	test.Assert(t, err == nil)
+	defer htc.Close()
+	t.Log(htc.Name())
+
+	req := &HTTPRequest{
+		Method: http.MethodGet,
+		Path:   "/BinaryEcho",
+	}
+	method, err := htc.getMethod(req)
+	test.Assert(t, err == nil, err)
+	test.Assert(t, method != nil)
+
+	ctx := context.Background()
+	sendMsg := initHttpSendMsg(transport.TTHeader)
+
+	// Marshal side
+	out := remote.NewWriterBuffer(256)
+	err = htc.Marshal(ctx, sendMsg, out)
+	test.Assert(t, err == nil, err)
+
+	// UnMarshal side
+	recvMsg := initHttpRecvMsg()
+	buf, err := out.Bytes()
+	test.Assert(t, err == nil, err)
+	recvMsg.SetPayloadLen(len(buf))
+	in := remote.NewReaderBuffer(buf)
+	err = htc.Unmarshal(ctx, recvMsg, in)
+	test.Assert(t, err == nil, err)
+}
+
+func initHttpSendMsg(tp transport.Protocol) remote.Message {
+	var req = &Args{
+		Request: &descriptor.HTTPRequest{
+			Method: http.MethodGet,
+			Path:   "/BinaryEcho",
+		},
+		Method: "BinaryEcho",
+	}
+	svcInfo := mocks.ServiceInfo()
+	ink := rpcinfo.NewInvocation("", "BinaryEcho")
+	ri := rpcinfo.NewRPCInfo(nil, nil, ink, nil, rpcinfo.NewRPCStats())
+	msg := remote.NewMessage(req, svcInfo, ri, remote.Call, remote.Client)
+	msg.SetProtocolInfo(remote.NewProtocolInfo(tp, svcInfo.PayloadCodec))
+	return msg
+}
+
+func initHttpRecvMsg() remote.Message {
+	var req = &Args{
+		Request: "Test",
+		Method:  "BinaryEcho",
+	}
+	ink := rpcinfo.NewInvocation("", "BinaryEcho")
+	ri := rpcinfo.NewRPCInfo(nil, nil, ink, nil, rpcinfo.NewRPCStats())
+	msg := remote.NewMessage(req, mocks.ServiceInfo(), ri, remote.Call, remote.Server)
+	return msg
 }
