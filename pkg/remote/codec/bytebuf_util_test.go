9babd23a0c3bac494868f58ebafb4bb09ab8295b
mxdblcf <mxdblcf@163.com>
2022-05-09 10:37:46 +0800

    test: add pkg/remote/codec unit test

--- pkg/remote/codec/bytebuf_util_test.go
+++ pkg/remote/codec/bytebuf_util_test.go
@@ -17,36 +17,39 @@
 package codec
 
 import (
-	"fmt"
 	"github.com/cloudwego/kitex/internal/test"
 	"github.com/cloudwego/kitex/pkg/remote"
 	"testing"
 )
 
+//test readUint32 with  byte array
 func TestReadUint32(t *testing.T) {
 	bytes := []byte{0x18, 0x2d, 0x44, 0x54}
 	bytes2, _ := Bytes2Uint32(bytes)
-	buffer := remote.NewReaderByteBuffer(bytes)
+	buffer := remote.NewReaderBuffer(bytes)
 	read, err := ReadUint32(buffer)
 	test.Assert(t, read == bytes2, err)
 }
 
+//test PeekUint32 with  byte array
 func TestPeekUint32(t *testing.T) {
 	bytes := []byte{0x18, 0x19, 0x44, 0x54}
-	buffer := remote.NewReaderByteBuffer(bytes)
+	buffer := remote.NewReaderBuffer(bytes)
 	read, err := PeekUint32(buffer)
 	bytes2, _ := Bytes2Uint32(bytes)
 	test.Assert(t, read == bytes2, err)
 }
 
+//test readUint16 with  byte array
 func TestReadUint16(t *testing.T) {
 	bytes := []byte{0x44, 0x54}
-	buffer := remote.NewReaderByteBuffer(bytes)
+	buffer := remote.NewReaderBuffer(bytes)
 	read, err := ReadUint16(buffer)
 	bytes2 := Bytes2Uint16NoCheck(bytes)
 	test.Assert(t, read == bytes2, err)
 }
 
+//test writeUint32 with  byte array
 func TestWriteUint32(t *testing.T) {
 	bytes := []byte{0x12, 0x34, 0x56, 0x78}
 	bytes2Uint32, _ := Bytes2Uint32(bytes)
@@ -57,6 +60,7 @@ func TestWriteUint32(t *testing.T) {
 	test.Assert(t, bytes2Uint32 == bytes2, err)
 }
 
+//test writeUint16 with  byte array
 func TestWriteUint16(t *testing.T) {
 	bytes := []byte{0x44, 0x23}
 	bytes2Uint16 := Bytes2Uint16NoCheck(bytes)
@@ -67,6 +71,7 @@ func TestWriteUint16(t *testing.T) {
 	test.Assert(t, bytes2Uint16 == bytes2, err)
 }
 
+//test writeByte with  byte
 func TestWriteByte(t *testing.T) {
 	var bytes byte = 'a'
 	out := remote.NewWriterBuffer(1)
@@ -75,6 +80,7 @@ func TestWriteByte(t *testing.T) {
 	test.Assert(t, bytes == next[0], err)
 }
 
+//test  Bytes2Uint32NoCheck with  byte array
 func TestBytes2Uint32NoCheck(t *testing.T) {
 
 	bytes := []byte{0x12, 0x34, 0x56, 0x78}
@@ -84,6 +90,7 @@ func TestBytes2Uint32NoCheck(t *testing.T) {
 	test.Assert(t, bytes2 == u)
 }
 
+//test  Bytes2Uint32 with  byte array
 func TestBytes2Uint32(t *testing.T) {
 	bytes := []byte{0x12, 0x34, 0x56, 0x78}
 	u := uint32(0x12345678)
@@ -94,6 +101,7 @@ func TestBytes2Uint32(t *testing.T) {
 	test.Assert(t, bytes2Uint32 == 0, err)
 }
 
+//test  Bytes2Uint16NoCheck with  byte array
 func TestBytes2Uint16NoCheck(t *testing.T) {
 	bytes := []byte{0x56, 0x78}
 	u := uint16(0x5678)
@@ -102,6 +110,7 @@ func TestBytes2Uint16NoCheck(t *testing.T) {
 	test.Assert(t, bytes2 == u)
 }
 
+//test  Bytes2Uint16 with  byte array
 func TestBytes2Uint16(t *testing.T) {
 
 	bytes := []byte{0x12, 0x34, 0x56, 0x78}
@@ -112,6 +121,7 @@ func TestBytes2Uint16(t *testing.T) {
 	test.Assert(t, b2 == 0, err1)
 }
 
+//test  Bytes2Uint8 with  byte array
 func TestBytes2Uint8(t *testing.T) {
 	bytes := []byte{0x12, 0x34, 0x56, 0x78}
 	u := uint8(0x78)
@@ -121,15 +131,7 @@ func TestBytes2Uint8(t *testing.T) {
 	test.Assert(t, b2 == 0, err1)
 }
 
-//todo This test encountered problems and found that the returned value was not the string length
-func TestReadString(t *testing.T) {
-	bytes := []byte("abcd")
-	inBuffer := remote.NewReaderByteBuffer(bytes)
-	readString, i, err := ReadString(inBuffer)
-	fmt.Println(i)
-	test.Assert(t, readString == "", err)
-}
-
+//test  WriteString with  string
 func TestWriteString(t *testing.T) {
 
 	val := "avvavv"
@@ -138,14 +140,10 @@ func TestWriteString(t *testing.T) {
 	test.Assert(t, writeString == 10, err)
 }
 
+//test  WriteString2Blen with  string
 func TestWriteString2BLen(t *testing.T) {
 	val := "bbbbbb"
 	buffer := remote.NewWriterBuffer(6)
 	writeString, err := WriteString2BLen(val, buffer)
 	test.Assert(t, writeString == 8, err)
-}
-
-//todo This test encountered problems and found that the returned value was not the string length
-func TestReadString2BLen(t *testing.T) {
-
 }
