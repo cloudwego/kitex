e061c0a7a1061cd2822042cf13430033d74b746c
Simon Wang <jellalleonhardt4869@gmail.com>
2022-06-28 14:42:01 +0800

    feat: add frugal thrift en/decoder (#470)
    Co-authored-by: lsjbd <44455379+lsjbd@users.noreply.github.com>

--- pkg/remote/codec/thrift/thrift.go
+++ pkg/remote/codec/thrift/thrift.go
@@ -31,21 +31,41 @@ import (
 	"github.com/cloudwego/kitex/pkg/stats"
 )
 
+// CodecType is config of the thrift codec. Priority: Frugal > FastMode > Normal
+type CodecType int
+
+const (
+	FastWrite CodecType = 1 << iota
+	FastRead
+)
+
 // NewThriftCodec creates the thrift binary codec.
 func NewThriftCodec() remote.PayloadCodec {
-	return &thriftCodec{}
+	return &thriftCodec{FastWrite | FastRead}
+}
+
+// NewThriftFrugalCodec creates the thrift binary codec powered by frugal.
+// Eg: xxxservice.NewServer(handler, server.WithPayloadCodec(thrift.NewThriftCodecWithConfig(thrift.FastWrite | thrift.FastRead)))
+func NewThriftCodecWithConfig(c CodecType) remote.PayloadCodec {
+	return &thriftCodec{c}
 }
 
 // NewThriftCodecDisableFastMode creates the thrift binary codec which can control if do fast codec.
 // Eg: xxxservice.NewServer(handler, server.WithPayloadCodec(thrift.NewThriftCodecDisableFastMode(true, true)))
 func NewThriftCodecDisableFastMode(disableFastWrite, disableFastRead bool) remote.PayloadCodec {
-	return &thriftCodec{disableFastWrite: disableFastWrite, disableFastRead: disableFastRead}
+	var c CodecType
+	if !disableFastRead {
+		c |= FastRead
+	}
+	if !disableFastWrite {
+		c |= FastWrite
+	}
+	return &thriftCodec{c}
 }
 
 // thriftCodec implements PayloadCodec
 type thriftCodec struct {
-	disableFastWrite bool
-	disableFastRead  bool
+	CodecType
 }
 
 // Marshal implements the remote.PayloadCodec interface.
@@ -60,12 +80,19 @@ func (c thriftCodec) Marshal(ctx context.Context, message remote.Message, out re
 		return err
 	}
 
+	// 2. encode thrift
+	// encode with hyper codec
+	if c.hasHyperMarshal(data) {
+		if err := c.hyperMarshal(data, message, out); err != nil {
+			return err
+		}
+		return nil
+	}
+
 	msgType := message.MessageType()
 	seqID := message.RPCInfo().Invocation().SeqID()
-
-	// 2. encode thrift
 	// encode with FastWrite
-	if !c.disableFastWrite {
+	if c.CodecType&FastWrite != 0 {
 		nw, nwOk := out.(remote.NocopyWrite)
 		if msg, ok := data.(thriftMsgFastCodec); ok && nwOk {
 			// nocopy write is a special implementation of linked buffer, only bytebuffer implement NocopyWrite do FastWrite
@@ -81,6 +108,7 @@ func (c thriftCodec) Marshal(ctx context.Context, message remote.Message, out re
 			return nil
 		}
 	}
+
 	// encode with normal way
 	tProt := NewBinaryProtocol(out)
 	if err := tProt.WriteMessageBegin(methodName, thrift.TMessageType(msgType), seqID); err != nil {
@@ -141,8 +169,31 @@ func (c thriftCodec) Unmarshal(ctx context.Context, message remote.Message, in r
 	}
 	// decode thrift
 	data := message.Data()
+
+	// decode with hyper unmarshal
+	if c.hasHyperMessageUnmarshal(data, message) {
+		msgBeginLen := bthrift.Binary.MessageBeginLength(methodName, msgType, seqID)
+		ri := message.RPCInfo()
+		internal_stats.Record(ctx, ri, stats.WaitReadStart, nil)
+		buf, err := tProt.next(message.PayloadLen() - msgBeginLen - bthrift.Binary.MessageEndLength())
+		internal_stats.Record(ctx, ri, stats.WaitReadFinish, err)
+		if err != nil {
+			return remote.NewTransError(remote.ProtocolError, err)
+		}
+		err = c.hyperMessageUnmarshal(buf, data)
+		if err != nil {
+			return err
+		}
+		err = tProt.ReadMessageEnd()
+		if err != nil {
+			return remote.NewTransError(remote.ProtocolError, err)
+		}
+		tProt.Recycle()
+		return nil
+	}
+
 	// decode with FastRead
-	if !c.disableFastRead {
+	if c.CodecType&FastRead != 0 {
 		if msg, ok := data.(thriftMsgFastCodec); ok && message.PayloadLen() != 0 {
 			msgBeginLen := bthrift.Binary.MessageBeginLength(methodName, msgType, seqID)
 			ri := message.RPCInfo()
@@ -164,6 +215,7 @@ func (c thriftCodec) Unmarshal(ctx context.Context, message remote.Message, in r
 			return err
 		}
 	}
+
 	// decode with normal way
 	switch t := data.(type) {
 	case MessageReader:
