49def5402edf2572dc9778fc0930a5a9753a7cc1
baiyutang <irichardwang@gmail.com>
2022-07-30 15:21:31 +0800

    feat(cmd example):usage func print cmd example

--- tool/cmd/kitex/args.go
+++ tool/cmd/kitex/args.go
@@ -45,6 +45,19 @@ type arguments struct {
 	extends []*extraFlag
 }
 
+const cmdExample = ` # Generate client codes when a project is in $GOPATH:
+  kitex {{path/to/IDL_file.thrift}}
+
+  # Generate client codes when a project is not in $GOPATH:
+  kitex -module {{github.com/xxx_org/xxx_name}} {{path/to/IDL_file.thrift}}
+
+  # Generate client codes with protobuf IDL:
+  kitex -type protobuf {{path/to/IDL_file.proto}}
+
+  # Generate server codes:
+  kitex -service {{svc_name}} {{path/to/IDL_file.thrift}}
+`
+
 func (a *arguments) addExtraFlag(e *extraFlag) {
 	a.extends = append(a.extends, e)
 }
@@ -97,8 +110,10 @@ func (a *arguments) buildFlags() *flag.FlagSet {
 		fmt.Fprintf(os.Stderr, `Version %s
 Usage: %s [flags] IDL
 
-flags:
-`, kitex.Version, os.Args[0])
+Examples:
+%s
+Flags:
+`, kitex.Version, os.Args[0], cmdExample)
 		f.PrintDefaults()
 		os.Exit(1)
 	}
