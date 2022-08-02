838bc006d72975a84142bb3ab396171e8aed9922
lushaojie <lushaojie@bytedance.com>
2022-08-02 15:29:56 +0800

    fix(tool): add license header

--- tool/internal_pkg/doc.go
+++ tool/internal_pkg/doc.go
@@ -1,3 +1,17 @@
+// Copyright 2022 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // internal_pkg contains pacakges for building the kitex command line tool.
 // APIs exported by packages under this directory do not promise any backward
 // compatibility, so please do not rely on them.
