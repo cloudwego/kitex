fb2f8285065a64301dc1ac99bb34c9b91c24c078
jayantxie <xiezhengyao@bytedance.com>
2022-10-08 11:58:52 +0800

    test: biz status errors

---
+++ pkg/kerrors/bizerrors_test.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kerrors
+
+import (
+	"testing"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/cloudwego/kitex/internal/test"
+)
+
+func TestBizStatusErrors(t *testing.T) {
+	bserr := NewBizStatusError(404, "not found")
+	test.Assert(t, bserr.BizStatusCode() == 404)
+	test.Assert(t, bserr.BizMessage() == "not found")
+	bserr = NewBizStatusErrorWithExtra(502, "bad gateway", nil)
+	berr := bserr.(*BizStatusError)
+	berr.SetBizExtra("key1", "value1")
+	berr.AppendBizMessage("[tag1]")
+	st, err := berr.GRPCStatus().WithDetails(&emptypb.Empty{})
+	test.Assert(t, err == nil)
+	berr.SetGRPCStatus(st)
+	test.Assert(t, bserr.BizStatusCode() == 502)
+	test.Assert(t, bserr.BizMessage() == "bad gateway [tag1]")
+	test.Assert(t, bserr.BizExtra()["key1"] == "value1")
+	test.Assert(t, bserr.Error() == "biz error: code=502, msg=bad gateway [tag1]")
+
+	err = bserr
+	_, ok := FromBizStatusError(err)
+	test.Assert(t, ok)
+	_, ok = FromBizStatusError(nil)
+	test.Assert(t, !ok)
+}
