beee636f970e99ca2b824b201e71c825ff57d325
kinggo <lilong.21@bytedance.com>
2022-11-23 00:19:23 +0800

    optimize: add .kitex to make sure the service is kitex server and align with hz

--- tool/internal_pkg/tpl/tool_version.go
+++ tool/internal_pkg/tpl/tool_version.go
@@ -14,21 +14,8 @@
 
 package tpl
 
-import (
-	"testing"
-
-	"github.com/cloudwego/kitex/internal/test"
-)
-
-func TestEmbedOK(t *testing.T) {
-	test.Assert(t, len(BootstrapTpl) > 0)
-	test.Assert(t, len(BuildTpl) > 0)
-	test.Assert(t, len(ClientTpl) > 0)
-	test.Assert(t, len(HandlerTpl) > 0)
-	test.Assert(t, len(HandlerMethodsTpl) > 0)
-	test.Assert(t, len(InvokerTpl) > 0)
-	test.Assert(t, len(MainTpl) > 0)
-	test.Assert(t, len(ServerTpl) > 0)
-	test.Assert(t, len(ServiceTpl) > 0)
-	test.Assert(t, len(ToolVersionTpl) > 0)
-}
+// ToolVersionTpl is the template for generating the .kitex source.
+var ToolVersionTpl string = `kitexinfo:
+   ServiceName: '{{.RealServiceName}}'
+   ToolVersion: '{{.Version}}'
+`
