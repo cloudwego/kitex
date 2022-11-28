bf70ffdd0099dae8078afbde8215283c589ed32e
Marina-Sakai <marina.sakai@bytedance.com>
2022-11-28 17:47:10 +0800

    fix bug

--- pkg/generic/descriptor/value_mapping.go
+++ pkg/generic/descriptor/value_mapping.go
@@ -18,6 +18,7 @@ package descriptor
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 )
 
@@ -44,9 +45,12 @@ var NewAPIJSConv NewValueMapping = func(value string) ValueMapping {
 // FIXME: compatible with old usages
 // we just return the origin val instead of return error
 func (m *apiJSConv) Request(ctx context.Context, val interface{}, field *FieldDescriptor) (interface{}, error) {
-	if field.HTTPMapping != nil {
+	fmt.Println("HIHIHIHIHI")
+	if field != nil && field.HTTPMapping != nil {
+		fmt.Println("NONONO")
 		return val, nil
 	}
+	fmt.Println("YESYSES")
 	switch v := val.(type) {
 	case string:
 		i, _ := strconv.ParseInt(v, 10, 64)
@@ -87,7 +91,7 @@ func (m *apiJSConv) Request(ctx context.Context, val interface{}, field *FieldDe
 }
 
 func (m *apiJSConv) Response(ctx context.Context, val interface{}, field *FieldDescriptor) (interface{}, error) {
-	if field.HTTPMapping != nil {
+	if field != nil && field.HTTPMapping != nil {
 		return val, nil
 	}
 	switch v := val.(type) {
