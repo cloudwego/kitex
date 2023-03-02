abc5ba06b7a25b47625fbc9394e3a64f384bd526
QihengZhou <qiheng.zhou@bytedance.com>
2023-03-02 13:57:08 +0800

    chore: revise the log for panic in OnRead (#843)

--- pkg/remote/trans/default_server_handler.go
+++ pkg/remote/trans/default_server_handler.go
@@ -125,7 +125,7 @@ func (t *svrTransHandler) OnRead(ctx context.Context, conn net.Conn) (err error)
 			if conn != nil {
 				ri := rpcinfo.GetRPCInfo(ctx)
 				rService, rAddr := getRemoteInfo(ri, conn)
-				klog.CtxErrorf(ctx, "KITEX: panic happened, close conn, remoteAddress=%s, remoteService=%s, error=%v\nstack=%s", rAddr, rService, panicErr, stack)
+				klog.CtxErrorf(ctx, "KITEX: panic happened, remoteAddress=%s, remoteService=%s, error=%v\nstack=%s", rAddr, rService, panicErr, stack)
 			} else {
 				klog.CtxErrorf(ctx, "KITEX: panic happened, error=%v\nstack=%s", panicErr, stack)
 			}
