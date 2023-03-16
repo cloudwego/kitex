9c9619054c1db8f0f9b679d4e155e2c450d994ab
Li2CO3 <45219850+HeyJavaBean@users.noreply.github.com>
2023-03-16 15:52:27 +0800

    chore(tool): add automatically deletion logic for kitex.yaml generated by v0.4.4 (#864)

--- tool/cmd/kitex/main.go
+++ tool/cmd/kitex/main.go
@@ -17,6 +17,7 @@ package main
 import (
 	"bytes"
 	"flag"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
@@ -84,5 +85,30 @@ NormalExit:
 			res = err.Error()
 		}
 		log.Warn("Adding apache/thrift@v0.13.0 to go.mod for generated code ..........", res)
+	}
+
+	// remove kitex.yaml generated from v0.4.4 which is renamed as kitex_info.yaml
+	if args.ServiceName != "" {
+		DeleteKitexYaml()
+	}
+}
+
+func DeleteKitexYaml() {
+	// try to read kitex.yaml
+	data, err := ioutil.ReadFile("kitex.yaml")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Warn("kitex.yaml, which is used to record tool info, is deprecated, it's renamed as kitex_info.yaml, you can delete it or ignore it.")
+		}
+		return
+	}
+	// if kitex.yaml exists, check content and delete it.
+	if strings.HasPrefix(string(data), "kitexinfo:") {
+		err = os.Remove("kitex.yaml")
+		if err != nil {
+			log.Warn("kitex.yaml, which is used to record tool info, is deprecated, it's renamed as kitex_info.yaml, you can delete it or ignore it.")
+		} else {
+			log.Warn("kitex.yaml, which is used to record tool info, is deprecated, it's renamed as kitex_info.yaml, so it's automatically deleted now.")
+		}
 	}
 }
