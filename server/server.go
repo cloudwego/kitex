73a16600dbc50e07699b615be1bc31e25de27ac5
Z.Q.K <qiankunzhang@aliyun.com>
2023-07-24 16:45:40 +0800

    no need to check svcInfo twice

--- server/server.go
+++ server/server.go
@@ -194,9 +194,6 @@ func (s *server) GetServiceInfo() *serviceinfo.ServiceInfo {
 
 // Run runs the server.
 func (s *server) Run() (err error) {
-	if s.svcInfo == nil {
-		return errors.New("no service, use RegisterService to set one")
-	}
 	if err = s.check(); err != nil {
 		return err
 	}
