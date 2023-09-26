dabe4d32b6ae47fd464af036996648f857a59ef6
Marina-Sakai <marina.sakai@bytedance.com>
2023-09-26 10:29:05 +0800

    fix: use compatible name

--- client/service_inline.go
+++ client/service_inline.go
@@ -65,7 +65,7 @@ type serviceInlineClient struct {
 type ServerInitialInfo interface {
 	Endpoints() endpoint.Endpoint
 	Option() *internal_server.Options
-	GetServices() map[string]*serviceinfo.Service
+	GetServiceInfo() map[string]*serviceinfo.Service
 }
 
 // NewServiceInlineClient creates a kitex.Client with the given ServiceInfo, it is from generated code.
@@ -78,7 +78,7 @@ func NewServiceInlineClient(svcInfo *serviceinfo.ServiceInfo, s ServerInitialInf
 	kc.opt = client.NewOptions(opts)
 	kc.serverEps = s.Endpoints()
 	kc.serverOpt = s.Option()
-	kc.serverOpt.RemoteOpt.SvcMap = s.GetServices()
+	kc.serverOpt.RemoteOpt.SvcMap = s.GetServiceInfo()
 	if err := kc.init(); err != nil {
 		_ = kc.Close()
 		return nil, err
