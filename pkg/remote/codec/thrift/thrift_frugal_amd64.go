0dd637830d323e2d904e754b25ee9dd1eb9b5d67
felix.fengmin <felix.fengmin@bytedance.com>
2024-01-20 18:39:45 +0800

    chore: frugal support go1.22

--- pkg/remote/codec/thrift/thrift_frugal_amd64.go
+++ pkg/remote/codec/thrift/thrift_frugal_amd64.go
@@ -1,5 +1,5 @@
-//go:build amd64 && !windows && go1.16 && !go1.22 && !disablefrugal
-// +build amd64,!windows,go1.16,!go1.22,!disablefrugal
+//go:build amd64 && !windows && go1.16 && !go1.23 && !disablefrugal
+// +build amd64,!windows,go1.16,!go1.23,!disablefrugal
 
 /*
  * Copyright 2021 CloudWeGo Authors
