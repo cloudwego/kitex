b1ac9125a94d49c61ec064e1150c7ef1012f9b39
Marina-Sakai <marina.sakai@bytedance.com>
2024-02-28 17:37:59 +0800

    chore: license

--- pkg/remote/codec/thrift/thrift_frugal.go
+++ pkg/remote/codec/thrift/thrift_frugal.go
@@ -2,7 +2,7 @@
 // +build !windows,go1.16,!go1.23,!disablefrugal
 
 /*
- * Copyright 2021 CloudWeGo Authors
+ * Copyright 2024 CloudWeGo Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
