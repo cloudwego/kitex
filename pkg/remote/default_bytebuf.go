4d27a5c4fb49e1bf5c8247801b842bccf58d91e0
qiheng.zhou <qiheng.zhou@bytedance.com>
2024-03-12 11:11:06 +0800

    test: supplement ut

--- pkg/remote/default_bytebuf.go
+++ pkg/remote/default_bytebuf.go
@@ -280,14 +280,6 @@ func (b *defaultByteBuffer) Bytes() (buf []byte, err error) {
 	return buf, nil
 }
 
-// BytesNocopy is used to get the bytes written with nocopy.
-func (b *defaultByteBuffer) BytesNocopy() (buf []byte, err error) {
-	if b.status&BitWritable == 0 {
-		return nil, errors.New("unwritable buffer, cannot support Bytes")
-	}
-	return b.buff[:b.writeIdx], nil
-}
-
 // NewBuffer returns a new writable remote.ByteBuffer.
 func (b *defaultByteBuffer) NewBuffer() ByteBuffer {
 	return NewWriterBuffer(256)
