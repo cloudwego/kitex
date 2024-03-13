c40a1167f36521da795b9fbc550ba923db8f965c
duanyi.aster <duanyi.aster@bytedance.com>
2024-03-13 17:59:49 +0800

    add convert

---
+++ pkg/utils/json_sonic.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import (
+	"runtime/debug"
+
+	"github.com/bytedance/sonic"
+	"github.com/cloudwego/kitex/pkg/klog"
+)
+
+var sonicConifg = sonic.Config{
+	EscapeHTML:     true,
+	ValidateString: true,
+}.Froze()
+
+// Map2JSONStr transform map[string]string to json str, perf is better than use json lib directly
+func Map2JSONStr(mapInfo map[string]string) (str string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			str, err = _Map2JSONStr(mapInfo)
+			klog.Warnf("KITEX: panic when Map2JSONStr, msg=%v, stack=%s", r, string(debug.Stack()))
+		}
+	}()
+	if len(mapInfo) == 0 {
+		return "{}", nil
+	}
+	return sonicConifg.MarshalToString(mapInfo)
+}
+
+// JSONStr2Map transform json str to map[string]string, perf is better than use json lib directly
+func JSONStr2Map(jsonStr string) (mapInfo map[string]string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			mapInfo, err = _JSONStr2Map(jsonStr)
+			klog.Warnf("KITEX: panic when JSONStr2Map, msg=%v, stack=%s", r, string(debug.Stack()))
+		}
+	}()
+	err = sonicConifg.UnmarshalFromString(jsonStr, &mapInfo)
+	if len(mapInfo) == 0 {
+		mapInfo = nil
+	}
+	return mapInfo, err
+}
