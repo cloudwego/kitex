d298f7ce52104756f0373a110afc0138970ac4a8
Marina-Sakai <marina.sakai@bytedance.com>
2024-05-30 14:54:24 +0800

    fix: nit things

--- pkg/remote/message.go
+++ pkg/remote/message.go
@@ -191,7 +191,6 @@ func (m *message) SpecifyServiceInfo(svcName, methodName string) (*serviceinfo.S
 		if mt := m.targetSvcInfo.MethodInfo(methodName); mt == nil {
 			return nil, NewTransErrorWithMsg(UnknownMethod, fmt.Sprintf("unknown method %s", methodName))
 		}
-		m.setMethodForGeneric(methodName)
 		return m.targetSvcInfo, nil
 	}
 	svcInfo := m.svcSearcher.SearchService(svcName, methodName, false)
@@ -199,7 +198,6 @@ func (m *message) SpecifyServiceInfo(svcName, methodName string) (*serviceinfo.S
 		return nil, NewTransErrorWithMsg(UnknownService, fmt.Sprintf("unknown service %s, method %s", svcName, methodName))
 	}
 	m.targetSvcInfo = svcInfo
-	m.setMethodForGeneric(methodName)
 	return svcInfo, nil
 }
 
@@ -281,15 +279,6 @@ func (m *message) SetPayloadCodec(pc PayloadCodec) {
 func (m *message) Recycle() {
 	m.zero()
 	messagePool.Put(m)
-}
-
-func (m *message) setMethodForGeneric(methodName string) {
-	if m.targetSvcInfo == nil {
-		return
-	}
-	if codec := m.targetSvcInfo.GenericCodec; codec != nil {
-		codec.SetMethod(methodName)
-	}
 }
 
 // TransInfo contains transport information.
--- pkg/serviceinfo/serviceinfo.go
+++ pkg/serviceinfo/serviceinfo.go
@@ -189,9 +189,7 @@ func (m methodInfo) StreamingMode() StreamingMode {
 	return m.streamingMode
 }
 
-// TODO: better design?
 type CodecInfo interface {
-	// TODO: gthrift.MessageReaderWriter or gproto.MessageReaderWriter
 	GetMessageReaderWriter() interface{}
 	GetIDLServiceName() string
 	SetMethod(method string)
