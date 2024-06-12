ca8e95e9a2c82843cac2a81e63bed65b168acae0
Marina-Sakai <marina.sakai@bytedance.com>
2024-06-12 10:06:46 +0800

    feat: grpc json generic

--- pkg/generic/generic_stream_service.go
+++ pkg/generic/generic_stream_service.go
@@ -20,38 +20,19 @@ import (
 	"github.com/cloudwego/kitex/pkg/serviceinfo"
 )
 
-// TODO: This is for generic server
-//type StreamingService interface {
-//	// ClientStreamingGenericCall handles the client streaming generic call
-//	ClientStreamingGenericCall(stream genericserver.ClientStreaming) (err error)
-//	// ServerStreamingGenericCall handles the server streaming generic call
-//	ServerStreamingGenericCall(req interface{}, stream genericserver.ServerStreaming) (err error)
-//	// BidirectionalStreamingGenericCall handles the bidirectional streaming generic call
-//	BidirectionalStreamingGenericCall(stream genericserver.BidirectionalStreaming) (err error)
-//}
-
 func StreamingServiceInfo(pcType serviceinfo.PayloadCodec, codec serviceinfo.CodecInfo) *serviceinfo.ServiceInfo {
 	return newClientStreamingServiceInfo(pcType, codec)
 }
 
-// TODO: refactor
 func newClientStreamingServiceInfo(pcType serviceinfo.PayloadCodec, codec serviceinfo.CodecInfo) *serviceinfo.ServiceInfo {
-	//handlerType := (*StreamingService)(nil)
-
-	// TODO: codec == nil check for binary generic
-	//var methods map[string]serviceinfo.MethodInfo
-	//var svcName string
-	//
-	//if codec == nil {
-	//	svcName = serviceinfo.GenericService
-	//	methods = map
-	//}
+	if codec == nil {
+		// TODO: support grpc binary generic
+		return nil
+	}
 
 	methods := map[string]serviceinfo.MethodInfo{
 		serviceinfo.GenericClientStreamingMethod: serviceinfo.NewMethodInfo(
-			// TODO: create handler when generic server supports grpc streaming
 			nil,
-			//clientStreamingCallHandler,
 			func() interface{} { return &Args{inner: codec.GetMessageReaderWriter()} },
 			func() interface{} { return &Result{inner: codec.GetMessageReaderWriter()} },
 			false,
@@ -59,7 +40,6 @@ func newClientStreamingServiceInfo(pcType serviceinfo.PayloadCodec, codec servic
 		),
 		serviceinfo.GenericServerStreamingMethod: serviceinfo.NewMethodInfo(
 			nil,
-			//serverStreamingCallHandler,
 			func() interface{} { return &Args{inner: codec.GetMessageReaderWriter()} },
 			func() interface{} { return &Result{inner: codec.GetMessageReaderWriter()} },
 			false,
@@ -67,7 +47,6 @@ func newClientStreamingServiceInfo(pcType serviceinfo.PayloadCodec, codec servic
 		),
 		serviceinfo.GenericBidirectionalStreamingMethod: serviceinfo.NewMethodInfo(
 			nil,
-			//bidirectionalStreamingCallHandler,
 			func() interface{} { return &Args{inner: codec.GetMessageReaderWriter()} },
 			func() interface{} { return &Result{inner: codec.GetMessageReaderWriter()} },
 			false,
@@ -89,72 +68,3 @@ func newClientStreamingServiceInfo(pcType serviceinfo.PayloadCodec, codec servic
 	svcInfo.Extra["generic"] = true
 	return svcInfo
 }
-
-// TODO: the followings are for generic server
-//func clientStreamingCallHandler(ctx context.Context, handler, arg, result interface{}) error {
-//	st := arg.(*streaming.Args).Stream
-//	stream := genericserver.NewClientStreamingServer(st)
-//	return handler.(StreamingService).ClientStreamingGenericCall(stream)
-//}
-//
-//func serverStreamingCallHandler(ctx context.Context, handler, arg, result interface{}) error {
-//	st := arg.(*streaming.Args).Stream
-//	stream := genericserver.NewServerStreamingServer(st)
-//	req := new(Args)
-//	if err := st.RecvMsg(req); err != nil {
-//		return err
-//	}
-//	return handler.(StreamingService).ServerStreamingGenericCall(req, stream)
-//}
-//
-//func bidirectionalStreamingCallHandler(ctx context.Context, handler, arg, result interface{}) error {
-//	st := arg.(*streaming.Args).Stream
-//	stream := genericserver.NewBidirectionalStreamingServer(st)
-//	return handler.(StreamingService).BidirectionalStreamingGenericCall(stream)
-//}
-
-/*
-svr, err := genericserver.NewStreamServer()
-svr.RegisterService("serviceName", handler)
-
-func (gs *genericServer) RegisterService(idlServiceName, streamingType, methodName[], handler) {
-	// TODO: create svcInfo
-	svr.RegisterService(svcInfo, handler)
-}
-
-svr.RegisterService(svcInfo, handler)
-
-/*
-func serviceInfo() *kitex.ServiceInfo {
-	return routeGuideServiceInfo
-}
-
-var routeGuideServiceInfo = NewServiceInfo()
-
-
-
-func NewServiceInfo() *kitex.ServiceInfo {
-	serviceName := "RouteGuide"
-	handlerType := (*routeguide.RouteGuide)(nil)
-	methods := map[string]kitex.MethodInfo{
-		"GetFeature":   kitex.NewMethodInfo(getFeatureHandler, newGetFeatureArgs, newGetFeatureResult, false),
-		"ListFeatures": kitex.NewMethodInfo(listFeaturesHandler, newListFeaturesArgs, newListFeaturesResult, false),
-		"RecordRoute":  kitex.NewMethodInfo(recordRouteHandler, newRecordRouteArgs, newRecordRouteResult, false),
-		"RouteChat":    kitex.NewMethodInfo(routeChatHandler, newRouteChatArgs, newRouteChatResult, false),
-	}
-	extra := map[string]interface{}{
-		"PackageName":     "routeguide",
-		"ServiceFilePath": ``,
-	}
-	extra["streaming"] = true
-	svcInfo := &kitex.ServiceInfo{
-		ServiceName:     serviceName,
-		HandlerType:     handlerType,
-		Methods:         methods,
-		PayloadCodec:    kitex.Protobuf,
-		KiteXGenVersion: "v1.13.3",
-		Extra:           extra,
-	}
-	return svcInfo
-}
-*/
