139897989a5b1d14db645971fa5876a8e6d4b6e7
Marina Sakai <118230951+Marina-Sakai@users.noreply.github.com>
2024-06-18 14:27:33 +0800

    refactor(generic): refactor existing generic to have new ServiceInfo which has the generic's reader and writer info directly (#1365)

--- pkg/generic/httppbthrift_codec_test.go
+++ pkg/generic/httppbthrift_codec_test.go
@@ -18,14 +18,16 @@ package generic
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 	"reflect"
 	"testing"
 
 	"github.com/bytedance/mockey"
 
 	"github.com/cloudwego/kitex/internal/test"
+	gthrift "github.com/cloudwego/kitex/pkg/generic/thrift"
 )
 
 // TestFromHTTPPbRequest this test is to make sure mockey can work.
@@ -34,11 +36,47 @@ func TestFromHTTPPbRequest(t *testing.T) {
 	mockey.PatchConvey("TestFromHTTPPbRequest", t, func() {
 		req, err := http.NewRequest("POST", "/far/boo", bytes.NewBuffer([]byte("321")))
 		test.Assert(t, err == nil)
-		mockey.Mock(ioutil.ReadAll).Return([]byte("123"), nil).Build()
+		mockey.Mock(io.ReadAll).Return([]byte("123"), nil).Build()
 		hreq, err := FromHTTPPbRequest(req)
 		test.Assert(t, err == nil)
 		test.Assert(t, reflect.DeepEqual(hreq.RawBody, []byte("123")), string(hreq.RawBody))
 		test.Assert(t, hreq.GetMethod() == "POST")
 		test.Assert(t, hreq.GetPath() == "/far/boo")
 	})
+}
+
+func TestHTTPPbThriftCodec(t *testing.T) {
+	p, err := NewThriftFileProvider("./httppb_test/idl/echo.thrift")
+	test.Assert(t, err == nil)
+	pbIdl := "./httppb_test/idl/echo.proto"
+	pbf, err := os.Open(pbIdl)
+	test.Assert(t, err == nil)
+	pbContent, err := io.ReadAll(pbf)
+	test.Assert(t, err == nil)
+	pbf.Close()
+	pbp, err := NewPbContentProvider(pbIdl, map[string]string{pbIdl: string(pbContent)})
+	test.Assert(t, err == nil)
+
+	htc := newHTTPPbThriftCodec(p, pbp)
+	defer htc.Close()
+	test.Assert(t, htc.Name() == "HttpPbThrift")
+
+	req, err := http.NewRequest("GET", "/Echo", bytes.NewBuffer([]byte("321")))
+	test.Assert(t, err == nil)
+	hreq, err := FromHTTPPbRequest(req)
+	test.Assert(t, err == nil)
+	// wrong
+	method, err := htc.getMethod("test")
+	test.Assert(t, err.Error() == "req is invalid, need descriptor.HTTPRequest" && method == nil)
+	// right
+	method, err = htc.getMethod(hreq)
+	test.Assert(t, err == nil, err)
+	test.Assert(t, method.Name == "Echo")
+	test.Assert(t, htc.svcName == "ExampleService")
+
+	rw := htc.getMessageReaderWriter()
+	_, ok := rw.(gthrift.MessageWriter)
+	test.Assert(t, ok)
+	_, ok = rw.(gthrift.MessageReader)
+	test.Assert(t, ok)
 }
