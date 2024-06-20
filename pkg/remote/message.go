bb740e196c9ad9caa448e58f88d0289a909f6c08
Marina-Sakai <marina.sakai@bytedance.com>
2024-06-20 10:52:33 +0800

    chore: add a comment and define service name for combine service

--- pkg/remote/message.go
+++ pkg/remote/message.go
@@ -195,7 +195,8 @@ func (m *message) SpecifyServiceInfo(svcName, methodName string) (*serviceinfo.S
 	}
 	// TODO: modify logic when supporting multi-service for generic
 	var key string
-	if svcName == "" || svcName == "CombineService" || svcName == "CombineService_" {
+	// when client does not pass svcName or uses combine service, fallback to searching for svcInfo by method name alone
+	if svcName == "" || svcName == serviceinfo.CombineService || svcName == serviceinfo.CombineService_ {
 		key = methodName
 	} else {
 		key = BuildMultiServiceKey(svcName, methodName)
