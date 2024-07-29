b01fdbc0dc07a51a3ee024654b1e7e305fb915e5
Marina-Sakai <marina.sakai@bytedance.com>
2024-07-29 17:42:25 +0800

    chore: move to internal

--- pkg/generic/thriftidl_provider.go
+++ pkg/generic/thriftidl_provider.go
@@ -27,8 +27,8 @@ import (
 	dthrift "github.com/cloudwego/dynamicgo/thrift"
 	"github.com/cloudwego/thriftgo/parser"
 
+	"github.com/cloudwego/kitex/internal/generic/thrift"
 	"github.com/cloudwego/kitex/pkg/generic/descriptor"
-	"github.com/cloudwego/kitex/pkg/generic/thrift"
 	"github.com/cloudwego/kitex/pkg/klog"
 )
 
