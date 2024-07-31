b2c165c4f53a1a401880b047a960d7dadf7c1c0b
shawn <xw2002121@gmail.com>
2024-07-31 20:18:35 +0800

    add test for command and flag

--- tool/internal_pkg/pluginmode/thriftgo/plugin.go
+++ tool/internal_pkg/pluginmode/thriftgo/plugin.go
@@ -119,10 +119,6 @@ func HandleRequest(req *plugin.Request) *plugin.Response {
 		files = append(files, fs...)
 	}
 
-	//if conv.Config.InitType != "" {
-	//	gen.GenerateCustomPackage(&conv.Package)
-	//}
-
 	res := &plugin.Response{
 		Warnings: conv.Warnings,
 	}
