eed574035e1699e4ad6fa6a89ce6297492348471
Marina Sakai <118230951+Marina-Sakai@users.noreply.github.com>
2024-08-30 16:27:09 +0800

    chore(generic): make generic streaming APIs internal (#1522)

--- client/genericclient/stream.go
+++ client/genericclient/stream.go
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+// NOTE: The basic generic streaming functions have been completed, but the interface needs adjustments. The feature is expected to be released later.
+
 package genericclient
 
 import (
@@ -46,11 +48,11 @@ type BidirectionalStreaming interface {
 	Recv() (resp interface{}, err error)
 }
 
-func NewStreamingClient(destService string, g generic.Generic, opts ...client.Option) (Client, error) {
-	return NewStreamingClientWithServiceInfo(destService, g, StreamingServiceInfo(g), opts...)
+func newStreamingClient(destService string, g generic.Generic, opts ...client.Option) (Client, error) {
+	return newStreamingClientWithServiceInfo(destService, g, StreamingServiceInfo(g), opts...)
 }
 
-func NewStreamingClientWithServiceInfo(destService string, g generic.Generic, svcInfo *serviceinfo.ServiceInfo, opts ...client.Option) (Client, error) {
+func newStreamingClientWithServiceInfo(destService string, g generic.Generic, svcInfo *serviceinfo.ServiceInfo, opts ...client.Option) (Client, error) {
 	var options []client.Option
 	options = append(options, client.WithGeneric(g))
 	options = append(options, client.WithDestService(destService))
@@ -105,7 +107,7 @@ type clientStreamingClient struct {
 	methodInfo serviceinfo.MethodInfo
 }
 
-func NewClientStreaming(ctx context.Context, genericCli Client, method string, callOpts ...callopt.Option) (ClientStreaming, error) {
+func newClientStreaming(ctx context.Context, genericCli Client, method string, callOpts ...callopt.Option) (ClientStreaming, error) {
 	gCli, ok := genericCli.(*genericServiceClient)
 	if !ok {
 		return nil, errors.New("invalid generic client")
@@ -140,7 +142,7 @@ type serverStreamingClient struct {
 	methodInfo serviceinfo.MethodInfo
 }
 
-func NewServerStreaming(ctx context.Context, genericCli Client, method string, req interface{}, callOpts ...callopt.Option) (ServerStreaming, error) {
+func newServerStreaming(ctx context.Context, genericCli Client, method string, req interface{}, callOpts ...callopt.Option) (ServerStreaming, error) {
 	gCli, ok := genericCli.(*genericServiceClient)
 	if !ok {
 		return nil, errors.New("invalid generic client")
@@ -176,7 +178,7 @@ type bidirectionalStreamingClient struct {
 	methodInfo serviceinfo.MethodInfo
 }
 
-func NewBidirectionalStreaming(ctx context.Context, genericCli Client, method string, callOpts ...callopt.Option) (BidirectionalStreaming, error) {
+func newBidirectionalStreaming(ctx context.Context, genericCli Client, method string, callOpts ...callopt.Option) (BidirectionalStreaming, error) {
 	gCli, ok := genericCli.(*genericServiceClient)
 	if !ok {
 		return nil, errors.New("invalid generic client")
