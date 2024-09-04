862a0bafba49e704838a703fea9ede04218e4f39
qiheng.zhou <qiheng.zhou@bytedance.com>
2024-09-04 15:53:52 +0800

    chore: lint

--- pkg/remote/trans/nphttp2/grpc/http2_client_test.go
+++ pkg/remote/trans/nphttp2/grpc/http2_client_test.go
@@ -1,14 +1,31 @@
+/*
+ * Copyright 2024 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package grpc
 
 import (
 	"context"
+	"math"
+	"testing"
+
 	"github.com/cloudwego/kitex/internal/test"
 	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/codes"
 	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/metadata"
 	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/status"
 	"golang.org/x/net/http2"
-	"math"
-	"testing"
 )
 
 func TestHttp2ClientTrailerRace(t *testing.T) {
