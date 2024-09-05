0cb53bf0af0459a98ed83f1f33654de6c329a9d7
Marina-Sakai <marina.sakai@bytedance.com>
2024-09-05 16:16:26 +0800

    fix: ignore statick check

--- pkg/generic/proto/type.go
+++ pkg/generic/proto/type.go
@@ -28,7 +28,7 @@ type (
 
 // Deprecated: this interface will be removed in v0.12.0
 //
-// nolint
+//lint:ignore SA1019 statick check error
 type Message interface {
 	Marshal() ([]byte, error)
 	TryGetFieldByNumber(fieldNumber int) (interface{}, error)
@@ -37,7 +37,7 @@ type Message interface {
 
 // Deprecated: this API will be removed in v0.12.0
 //
-// nolint
+//lint:ignore SA1019 static check error
 func NewMessage(descriptor MessageDescriptor) Message {
 	return dynamic.NewMessage(descriptor)
 }
