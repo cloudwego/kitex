e45145aac723b381c88994423dc4af6045301dec
Joway <wangzhuowei@bytedance.com>
2024-10-17 17:42:27 +0800

    chore(ttstream): fix ci (#1579)

--- server/streamxserver/server_option.go
+++ server/streamxserver/server_option.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package streamxserver
 
 import (
@@ -8,8 +24,10 @@ import (
 	"github.com/cloudwego/kitex/server"
 )
 
-type Option internal_server.Option
-type Options = internal_server.Options
+type (
+	Option  internal_server.Option
+	Options = internal_server.Options
+)
 
 func WithListener(ln net.Listener) Option {
 	return ConvertNativeServerOption(server.WithListener(ln))
