584fa425a3245a2800ac01dc3a2e150c95e3643b
Joway <wangzhuowei@bytedance.com>
2024-11-06 13:42:20 +0800

    chore: add transport buffer test (#1605)

---
+++ pkg/streamx/provider/ttstream/transport_buffer_test.go
@@ -0,0 +1,79 @@
+//go:build !windows
+
+/*
+ * Copyright 2024 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ttstream
+
+import (
+	"testing"
+
+	"github.com/cloudwego/kitex/internal/test"
+	"github.com/cloudwego/netpoll"
+)
+
+func TestTransportBuffer(t *testing.T) {
+	rfd, wfd := netpoll.GetSysFdPairs()
+	rconn, err := netpoll.NewFDConnection(rfd)
+	test.Assert(t, err == nil, err)
+	wconn, err := netpoll.NewFDConnection(wfd)
+	test.Assert(t, err == nil, err)
+
+	rbuf := newReaderBuffer(rconn.Reader())
+	wbuf := newWriterBuffer(wconn.Writer())
+	msg := make([]byte, 1024)
+	for i := 0; i < len(msg); i++ {
+		msg[i] = 'a' + byte(i%26)
+	}
+
+	// test Malloc
+	buf, err := wbuf.Malloc(len(msg))
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, wbuf.WrittenLen(), len(msg))
+	copy(buf, msg)
+	err = wbuf.Flush()
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, wbuf.WrittenLen(), 0)
+
+	// test ReadBinary
+	buf = make([]byte, len(msg))
+	n, err := rbuf.ReadBinary(buf)
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, buf, msg)
+	test.DeepEqual(t, n, len(msg))
+	test.DeepEqual(t, rbuf.ReadLen(), len(msg))
+	err = rbuf.Release(nil)
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, rbuf.ReadLen(), 0)
+
+	// test WriteBinary
+	n, err = wbuf.WriteBinary(msg)
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, n, len(msg))
+	test.DeepEqual(t, wbuf.WrittenLen(), len(msg))
+	err = wbuf.Flush()
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, wbuf.WrittenLen(), 0)
+
+	// test Next
+	buf, err = rbuf.Next(len(msg))
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, buf, msg)
+	test.DeepEqual(t, rbuf.ReadLen(), len(msg))
+	err = rbuf.Release(nil)
+	test.Assert(t, err == nil, err)
+	test.DeepEqual(t, rbuf.ReadLen(), 0)
+}
