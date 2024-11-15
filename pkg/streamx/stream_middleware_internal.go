196638e30ab208d1e2e390f728e9d7a94659e912
Joway <wangzhuowei@bytedance.com>
2024-11-15 14:16:27 +0800

    feat: using original trace ctx (#1613)

--- pkg/streamx/stream_middleware_internal.go
+++ pkg/streamx/stream_middleware_internal.go
@@ -22,21 +22,31 @@ import (
 	"github.com/cloudwego/kitex/pkg/stats"
 )
 
-func NewStreamRecvStatMiddleware(ehandler EventHandler) StreamRecvMiddleware {
+func NewStreamRecvStatMiddleware(traceCtx context.Context, ehandler EventHandler) StreamRecvMiddleware {
 	return func(next StreamRecvEndpoint) StreamRecvEndpoint {
 		return func(ctx context.Context, stream Stream, res any) (err error) {
 			err = next(ctx, stream, res)
-			ehandler(ctx, stats.StreamRecv, err)
+			// if traceCtx is nil, using the current ctx
+			// otherwise, we should use the original trace ctx instead
+			if traceCtx == nil {
+				traceCtx = ctx
+			}
+			ehandler(traceCtx, stats.StreamRecv, err)
 			return err
 		}
 	}
 }
 
-func NewStreamSendStatMiddleware(ehandler EventHandler) StreamSendMiddleware {
+func NewStreamSendStatMiddleware(traceCtx context.Context, ehandler EventHandler) StreamSendMiddleware {
 	return func(next StreamSendEndpoint) StreamSendEndpoint {
 		return func(ctx context.Context, stream Stream, res any) (err error) {
 			err = next(ctx, stream, res)
-			ehandler(ctx, stats.StreamSend, err)
+			// if traceCtx is nil, using the current ctx
+			// otherwise, we should use the original trace ctx instead
+			if traceCtx == nil {
+				traceCtx = ctx
+			}
+			ehandler(traceCtx, stats.StreamSend, err)
 			return err
 		}
 	}
