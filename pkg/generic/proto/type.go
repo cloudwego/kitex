95f6ac3437dd7512cc5042e6806344870f4e5c51
Marina Sakai <118230951+Marina-Sakai@users.noreply.github.com>
2024-11-20 15:26:30 +0800

    chore(generic): restore proto Message back (#1619)

--- pkg/generic/proto/type.go
+++ pkg/generic/proto/type.go
@@ -18,9 +18,22 @@ package proto
 
 import (
 	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/dynamic"
 )
 
 type (
 	ServiceDescriptor = *desc.ServiceDescriptor
 	MessageDescriptor = *desc.MessageDescriptor
 )
+
+// Deprecated: please use github.com/jhump/protoreflect/dynamic Message instead
+type Message interface {
+	Marshal() ([]byte, error)
+	TryGetFieldByNumber(fieldNumber int) (interface{}, error)
+	TrySetFieldByNumber(fieldNumber int, val interface{}) error
+}
+
+// Deprecated: please use github.com/jhump/protoreflect/dynamic NewMessage instead
+func NewMessage(descriptor MessageDescriptor) Message {
+	return dynamic.NewMessage(descriptor)
+}
