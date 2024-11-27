e98fd88b007b89f62de5f68af19d76a5574d7acf
Kyle Xiao <xiaoguoqiao@bytedance.com>
2024-11-27 14:03:08 +0800

    perf(client): new rpctimeout impl (#1581)

---
+++ client/rpctimeout_pool_test.go
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2021 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package client
+
+import (
+	"context"
+	"errors"
+	"runtime"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/cloudwego/kitex/internal/test"
+)
+
+func TestTimeoutPool(t *testing.T) {
+	ctx := context.Background()
+	maxIdle := 2
+	maxIdleTime := 100 * time.Millisecond
+	p := newTimeoutPool(maxIdle, maxIdleTime)
+	var (
+		sum  int32
+		wg   sync.WaitGroup
+		size int32 = 100
+	)
+	test.Assert(t, p.Size() == 0)
+	for i := int32(0); i < size; i++ {
+		wg.Add(1)
+		go func() {
+			ctx, err := p.RunTask(ctx, time.Second,
+				nil, nil, func(ctx context.Context, req, resp interface{}) error {
+					defer wg.Done()
+					atomic.AddInt32(&sum, 1)
+					return nil
+				})
+			test.Assert(t, err == nil && ctx.Err() == nil)
+		}()
+	}
+	wg.Wait()
+	test.Assert(t, atomic.LoadInt32(&sum) == size)
+	test.Assert(t, p.Size() != 0)
+	time.Sleep(2 * maxIdleTime)
+	test.Assert(t, p.Size() == 0, p.Size())
+}
+
+func noop(ctx context.Context, req, resp interface{}) error { return nil }
+
+func BenchmarkTimeoutPool(b *testing.B) {
+	maxIdleWorkers := runtime.GOMAXPROCS(0)
+	ctx := context.Background()
+	p := newTimeoutPool(maxIdleWorkers, 10*time.Millisecond)
+	b.RunParallel(func(b *testing.PB) {
+		for b.Next() {
+			p.RunTask(ctx, time.Second, nil, nil, noop)
+			for int(p.Size()) > maxIdleWorkers {
+				runtime.Gosched()
+			}
+		}
+	})
+}
+
+func TestTask(t *testing.T) {
+	t.Run("ReturnErr", func(t *testing.T) {
+		returnErr := errors.New("ep error")
+		ctx := context.Background()
+		timeout := 20 * time.Millisecond
+		p := newTimeoutTask(ctx, timeout, nil, nil,
+			func(ctx context.Context, _, _ any) error {
+				time.Sleep(timeout / 2)
+				return returnErr
+			})
+
+		go p.Run()
+		ctx, err := p.Wait()
+		test.Assert(t, returnErr == err, err)
+		test.Assert(t, ctx.Err() == nil, ctx.Err())
+	})
+
+	t.Run("Timeout", func(t *testing.T) {
+		ctx := context.Background()
+		timeout := 50 * time.Millisecond
+		dl := time.Now().Add(timeout)
+		var returned atomic.Value
+		p := newTimeoutTask(ctx, timeout, nil, nil, func(ctx context.Context, _, _ any) error {
+			d, ok := ctx.Deadline()
+			test.Assert(t, ok)
+			test.Assert(t, d.Sub(dl) < timeout/4)
+			<-ctx.Done()
+			returned.Store(true)
+			time.Sleep(timeout / 4)
+			return errors.New("ep error")
+		})
+
+		t0 := time.Now()
+		go p.Run()
+		ctx, err := p.Wait()
+		t1 := time.Now()
+		test.Assert(t, errors.Is(err, context.DeadlineExceeded), err)
+		test.Assert(t, errors.Is(ctx.Err(), context.DeadlineExceeded), ctx.Err())
+		test.Assert(t, t1.Sub(t0)-timeout < timeout/4)
+		time.Sleep(timeout / 2)
+		test.Assert(t, returned.Load() != nil)
+	})
+
+	t.Run("ParentCtxDone", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		p := newTimeoutTask(ctx, 0, nil, nil,
+			func(ctx context.Context, _, _ any) error {
+				cancel()
+				time.Sleep(10 * time.Millisecond)
+				return nil
+			})
+
+		go p.Run()
+		ctx, err := p.Wait()
+		test.Assert(t, err == context.Canceled, err)
+		test.Assert(t, errors.Is(ctx.Err(), context.Canceled), ctx.Err())
+	})
+
+	t.Run("Panic", func(t *testing.T) {
+		ctx := context.Background()
+		timeout := 20 * time.Millisecond
+		p := newTimeoutTask(ctx, timeout, nil, nil,
+			func(ctx context.Context, _, _ any) error {
+				panic("testpanic")
+			})
+
+		go p.Run()
+		ctx, err := p.Wait()
+		test.Assert(t, err != nil && strings.Contains(err.Error(), "testpanic"), err)
+		test.Assert(t, ctx.Err() == nil)
+	})
+}
