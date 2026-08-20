72ad8a8db57e53846c656e56dce4e3c873d86161
Scout Wang <yuxuan.wang1@bytedance.com>
2026-08-20 16:49:31 +0800

    feat(grpc): support identifying cascaded cancel (#1987)

--- pkg/remote/trans/nphttp2/stream_test.go
+++ pkg/remote/trans/nphttp2/stream_test.go
@@ -19,11 +19,26 @@ package nphttp2
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/cloudwego/kitex/internal/test"
+	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/codes"
 	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/grpc"
+	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/status"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/pkg/serviceinfo"
 )
+
+type cascadeCancelContext struct {
+	context.Context
+	err error
+}
+
+// Err models the process-local cancellation reason exposed by the gRPC server
+// context when it directly drives an outbound client-side stream.
+func (c *cascadeCancelContext) Err() error {
+	return c.err
+}
 
 func TestStream(t *testing.T) {
 	// init
@@ -60,4 +75,40 @@ func TestStream(t *testing.T) {
 	// test Close()
 	err = stream.GetGRPCStream().Close()
 	test.Assert(t, err == nil, err)
+}
+
+func TestClientStreamRecvMsgCascadeCancel(t *testing.T) {
+	reason := status.Err(codes.Canceled, "inbound RPC terminated")
+	parent, cancel := context.WithCancel(context.Background())
+	cancel()
+	ctx := rpcinfo.NewCtxWithRPCInfo(
+		&cascadeCancelContext{Context: parent, err: reason},
+		newMockRPCInfo(serviceinfo.StreamingBidirectional),
+	)
+
+	opt := newMockClientOption(nil)
+	defer opt.ConnPool.Close()
+	conn, err := opt.ConnPool.Get(ctx, "tcp", mockAddr0, newMockConnOption())
+	test.Assert(t, err == nil, err)
+
+	handler, err := NewCliTransHandlerFactory().NewTransHandler(opt)
+	test.Assert(t, err == nil, err)
+	stream := NewClientStream(ctx, nil, conn, handler)
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- stream.RecvMsg(ctx, &HelloRequest{})
+	}()
+
+	select {
+	case err = <-errCh:
+	case <-time.After(time.Second):
+		t.Fatal("RecvMsg did not return after cascade cancellation")
+	}
+
+	st, ok := status.FromError(err)
+	test.Assert(t, ok, err)
+	test.Assert(t, st.IsCascadeCancel())
+	test.Assert(t, st.Code() == codes.Canceled, st.Code())
+	test.Assert(t, st.Message() == "inbound RPC terminated", st.Message())
 }
